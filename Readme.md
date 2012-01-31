822221542d469a083995e14a6609d073346976a5
Robin Levin <robin@gradecam.com>
2012-01-31 09:31:36 -0800

    Taxonomy first commit

---
+++ Readme.md
@@ -0,0 +1,176 @@
+_taxonomy_ is a library for manipulating tree structures in [node](http://nodejs.org). The tree object it creates can be used with jstree on the front end if so desired. Because it tries to be interoperable with jstree, the root.data property on the tree itself is an array of objects.
+
+## Example
+
+Require taxonomy.js .. assuming in node.js env:
+
+    var tax = require('../lib/taxonomy.js');
+
+Now build up your tree passing in the node and it's optional parent. If parent is not provided it is assumed you want the node to hang of the root of your tree:
+
+    var firstNode = tax.addNode({"data":"foo"}, null);
+    var childNode = tax.addNode({"data":"bar"}, firstNode);
+    var grandchildNode = tax.addNode({"data":"grandchild"}, childNode);
+    var greatGrandchildNode = tax.addNode({"data":"greatGrandchild"}, grandchildNode);
+    var tree = tax.getTree().data;
+
+    console.log(JSON.stringify(tree, null, '  '));
+    /* outputs:
+    [
+      {
+        "data": "foo",
+        "_id": "tax_4",
+        "children": [
+          {
+            "data": "bar",
+            "_id": "tax_5",
+            "children": [
+              {
+                "data": "grandchild",
+                "_id": "tax_6",
+                "children": [
+                  {
+                    "data": "greatGrandchild",
+                    "_id": "tax_7",
+                    "children": [],
+                    "isLeaf": true
+                  }
+                ],
+                "isLeaf": false
+              }
+            ],
+            "isLeaf": false
+          }
+        ],
+        "isLeaf": false
+      }
+    ]
+    */
+
+## createNode and addNode
+
+Adding nodes inline in addNode (like in the above example) is not the preferred way to create nodes. The method createNode is a convenience method which takes an array of leaf children (null in following example), and data property's value:
+
+    var node = tax.createNode(['foo','bar','qux'], 'mydata');
+
+So above we have 'foo', 'bar', and 'baz' as child leafs of node and 'mydata' as the value of node's data property. Here's another example creating a nested tree where the nodes have no leaf children (because null is passed):
+
+    var p    = tax.createNode(null, "parent");
+    var c    = tax.createNode(null, "child");
+    var g    = tax.createNode(null, "grandchild");
+    var gg   = tax.createNode(null, "ggrandchild");
+    tax.addNode( gg, tax.addNode(g, tax.addNode(c, tax.addNode(p, null))));
+    console.log(JSON.stringify(tax.getTree(), null, '  '));
+    /* outputs:
+    {
+      "data": [
+        {
+          "children": [
+            {
+              "children": [
+                {
+                  "children": [
+                    {
+                      "children": [],
+                      "data": "ggrandchild",
+                      "_id": "tax_46",
+                      "isLeaf": true
+                    }
+                  ],
+                  "data": "grandchild",
+                  "_id": "tax_44",
+                  "isLeaf": false
+                }
+              ],
+              "data": "child",
+              "_id": "tax_41",
+              "isLeaf": false
+            }
+          ],
+          "data": "parent",
+          "_id": "tax_40",
+          "isLeaf": false
+        }
+      ]
+    }
+    */
+
+## Get the path of a node
+
+Assuming gg is a great grandchild node you can pass it's id and get back it's unix style path:
+
+    path = tax.path(gg._id);
+    path.should.eql('/parent/child/grandchild/');
+
+NOTE: You'll notice from the example above that I've chosen to omit the basename itself. This allows you to choose an arbitrary basename (e.g. if you have a "slug" that's different than the data property, etc.). Of course, you can easily append it if that's what you'd like to achieve.
+
+Tested to work with deeply nested paths:
+
+    var path = null, i, n, arr;
+    arr = [];
+    for(i=1; i<=10;i++) {
+        n = tax.createNode(null, i);
+        tax.addNode(n, arr.pop());
+        arr.push(n);
+    }
+    path = tax.path(arr.pop()._id);
+    path.should.eql('/1/2/3/4/5/6/7/8/9/');
+
+## Inserting nodes
+
+You can insert nodes as children of a parent node at either the end or optional index provided:
+
+    tax.insert(child, parent._id, 2);
+
+## Removing nodes
+
+You can remove a node by passing in it's id. Subsequently, the deleted node's parent's isLeaf property will go from being true to false (if deleted node was parent's only child of course!)
+
+    tax.remove(grandchildNode._id);
+    child.isLeaf.should.eql(true);
+
+## Arbitrary properties are retained 
+
+The only "required" property of a node is 'data'. If not provided to createNode (or you create node inline in call to addNode), it will throw RequireFieldError. However, once you've met this requirement you can add arbitrary fields. For example, here are some jstree-specific properties being added to a node:
+
+    var anode = tax.createNode(['leaf1','leaf2','leaf3'], 'data_here',
+        // and here are some arbitrary jstree fields which will be preserved:
+        {'attr':{'href':'#',
+         'id':'me_id'},
+         "state":"closed", 
+         "metadata" : "a string, array, object, etc",
+         "title" : "My Title" });
+
+## Installation
+
+    $ npm install taxonomy
+
+## Tests
+
+    $ npm install --dev
+    $ mocha -R list
+
+## License 
+
+(The MIT License)
+
+Copyright (c) 2012 Rob Levin &lt;roblevintennis@gmail.com&gt;
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+'Software'), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ test/taxonomy.test.js
@@ -0,0 +1,238 @@
+var should = require('should');
+var tax    = require('../lib/taxonomy.js');
+
+// To run:
+// <path>/taxonomy $ mocha -R list
+describe('Taxonomy', function() {
+    var jstree = null;
+    beforeEach(function(done){
+        done();
+    });
+    afterEach(function(done){
+        tax.clear();
+        done();
+    });
+    describe('Create tree', function() {
+        it('should be able to add a root node', function(done) {
+            var first = tax.addNode({"data":"bar"}, null);
+            first.should.have.property("data", "bar");
+            tax.getTree().data.length.should.equal(1);
+            tax.getTree().data[0].should.have.property("data","bar");
+            done();
+        });
+        it('should be able to add a root and children nodes', function(done) {
+            var firstNode = tax.addNode({"data":"foo"}, null);
+            var childNode = tax.addNode({"data":"bar"}, firstNode);
+            var tree = tax.getTree().data;
+            tree[0].children[0].should.have.property('data','bar');
+            var secondChildNode = tax.addNode({"data":"child2"}, firstNode);
+            tree[0].children[1].should.have.property('data','child2');
+            done();
+        });
+        it('should be able to add children of children', function(done) {
+            var firstNode = tax.addNode({"data":"foo"}, null);
+            var childNode = tax.addNode({"data":"bar"}, firstNode);
+            var grandchildNode = tax.addNode({"data":"grandchild"}, childNode);
+            var greatGrandchildNode = tax.addNode({"data":"greatGrandchild"}, grandchildNode);
+            var tree = tax.getTree().data;
+            tree[0].children[0].should.have.property('data','bar');
+            tree[0].children[0].children[0].should.be.a('object').and.have.property('data', 'grandchild');
+            tree[0].children[0].children[0].should.not.have.property('data', 'nope');
+            tree[0].children[0].children[0].children[0].should.have.property('data','greatGrandchild');
+            done();
+        });
+        it('should create jstree with mulitple root nodes', function(done) {
+            tax.addNode({"data":"a","attr": {"id":"aid"},"children":["achild1","achild2"]});
+            tax.addNode({"data":"b","attr": {"id":"bid"},"children":["bchild1","bchild2"]});
+            tax.addNode({"data":"c","attr": {"id":"cid"},"children":["cchild1","cchild2"]});
+            var tree = tax.getTree();
+            var data = tree.data;
+            data[0].data.should.equal('a');
+            data[1].data.should.equal('b');
+            data[2].data.should.equal('c');
+            data[2].children[1].should.equal('cchild2');
+            done();
+        });
+        it('should return all roots', function(done) {
+            tax.addNode({"data":"a","attr": {"id":"aid"},"children":["achild1","achild2"]});
+            tax.addNode({"data":"b","attr": {"id":"bid"},"children":["bchild1","bchild2"]});
+            var roots = tax.getRoots();
+            roots.length.should.eql(2);
+            roots[1].attr.id.should.eql('bid');
+            roots[1].attr.id.should.not.eql('bogus');
+            done();
+        });
+    });
+
+    describe('Create node', function() {
+        it('should be able to create a node with data and children', function(done) {
+            var anode = tax.createNode(['foo','bar','baz'], 'some data');
+            anode.should.have.property("data", "some data");
+            anode.children.should.eql(['foo','bar','baz']);
+            done();
+        });
+        it('should be able to create a node and any arbitrary properties should be presevered', function(done) {
+            var anode = tax.createNode(['foo','bar','baz'], 'mydata', 
+                {'attr':{'href':'#','id':'me_id'},
+                 "state":"closed", 
+                 "metadata" : "a string, array, object, etc",
+                 "title" : "My TiTle" });
+            anode.attr.should.eql({'href':'#','id':'me_id'});
+            anode.state.should.eql('closed');
+            anode.metadata.should.eql('a string, array, object, etc');
+            anode.title.should.eql('My TiTle');
+            var firstNode = tax.addNode(anode, null);
+            var childNode = tax.addNode({"data":"bar"}, firstNode);
+            var tree = tax.getTree().data;
+            tree[0].children[3].should.have.property('data','bar');
+            done();
+        });
+        it('should be disallow adding node that has no data: property', function(done) {
+            var nodata = null;
+            // Can't create node with no data property
+            try {
+                nodata = tax.createNode(['bar']);
+            } catch(e) {
+                e.name.should.eql('RequireFieldError');
+                e.message.should.match(/.*require.*/i);
+            }
+
+            // Can't add a node with no data property. Can only happen if node 
+            // provided is created inline
+            try {
+                var anode = tax.createNode(null, 'foo');
+                tax.addNode(anode, null);
+                tax.addNode({'nodata':'ohoh!'}, anode);
+            } catch(e) {
+                e.name.should.eql('RequireFieldError');
+            }
+            done();
+        });
+    });
+
+    describe('Visit related operations', function() {
+        it('should be able to find a child in node by id', function(done) {
+            var anode = tax.createNode(['foo','bar','baz'], 'some data');
+            var bnode = tax.createNode(['wang','jang','bang'], 'some more data');
+            var cnode = tax.createNode(['dang','fang','bang'], 'some more data');
+            tax.addNode(anode, null);
+            tax.addNode(bnode, null);
+            tax.addNode(cnode, anode);
+            var match = tax.find(cnode._id);
+            match.should.eql(cnode);
+            // Go a few levels deeper ;)
+            var dnode = tax.createNode(['do','re','me'], 'music baby');
+            var enode = tax.createNode(['fa','so','la'], 'yeah');
+            var fnode = tax.createNode(['ti','do','re'], 'oh yeah');
+            tax.addNode(dnode, bnode);
+            tax.addNode(enode, dnode);
+            tax.addNode(fnode, enode);
+            match = null;
+            match = tax.find(fnode._id);
+            match.should.eql(fnode);
+            done();
+        });
+        it('should be able to insert child node after parent', function(done) {
+            var parent = tax.createNode(['foo'], 'some data');
+            tax.addNode(parent, null);
+            var child = tax.createNode(['bar'],'bar data');
+            tax.insert(child, parent._id);
+            var tree = tax.getTree().data;
+            tree[0].children[1].should.eql(child);
+            tax.find(parent._id).children[1].should.eql(child);
+            done();
+        });
+        it('should be able to insert child node after parent at index', function(done) {
+            var parent = tax.createNode(['foo','bar','qux'], 'some data');
+            tax.addNode(parent, null);
+            var child = tax.createNode(['bar'],'bar data');
+            tax.insert(child, parent._id, 2);
+            tax.find(parent._id).children.length.should.eql(4);
+            tax.find(parent._id).children[2].should.eql(child);
+            done();
+        });
+        it('should be able to find parent and child object from child id', function(done) {
+            var parent = tax.createNode(['foo','bar','qux'], 'some data');
+            tax.addNode(parent, null);
+            var child = tax.createNode(['bar'],'bar data');
+            tax.insert(child, parent._id, 2);
+            var pc = tax.findParentChild(child._id);
+            pc.child.should.eql(child);
+            pc.parent.should.eql(parent);
+            done();
+        });
+        it('should be able to insert node object', function(done) {
+            var parent = tax.createNode(['foo','bar','qux'], 'some data');
+            tax.addNode(parent, null);
+            var child = tax.createNode(['bar'],'bar data');
+            tax.insert(child, parent._id, 2);
+            var removed = tax.remove(child._id);
+            removed.should.eql(true);
+            removed = tax.remove('bogux_xxx');
+            removed.should.eql(false);
+            done();
+        });
+        it('should be able to determine if leaf', function(done) {
+            var parent = tax.createNode(['foo','bar','qux'], 'some data');
+            tax.addNode(parent, null);
+            var child = tax.createNode(null,'bar data');
+            tax.insert(child, parent._id, 2);
+            parent.isLeaf.should.eql(false);
+            child.isLeaf.should.eql(true);
+            var grandchildNode = tax.addNode({"data":"grandchild"}, child);
+            grandchildNode.isLeaf.should.eql(true);
+            tax.addNode({"data":"great_grandchild"}, grandchildNode);
+            grandchildNode.isLeaf.should.eql(false);
+            done();
+        });
+        it('should change isleaf upon removal', function(done) {
+            var parent = tax.createNode(['foo','bar','qux'], 'some data');
+            tax.addNode(parent, null);
+            var child = tax.createNode(null,'bar data');
+            tax.insert(child, parent._id, 2);
+            var grandchildNode = tax.addNode({"data":"grandchild"}, child);
+            grandchildNode.isLeaf.should.eql(true);
+            tax.addNode({"data":"great_grandchild"}, grandchildNode);
+            // Before removal, child.isLeaf==false as it still has grandchildren
+            child.isLeaf.should.eql(false);
+            // However, after removing grandchild, child.isLeaf will eq true
+            tax.remove(grandchildNode._id);
+            child.isLeaf.should.eql(true);
+            done();
+        });
+        it('should be able to get the path of a node', function(done) {
+            var p    = tax.createNode(null, "parent");
+            var c    = tax.createNode(null, "child");
+            var c2   = tax.createNode(null, "child2");
+            var c3   = tax.createNode(null, "child3");
+            var g    = tax.createNode(null, "grandchild");
+            var g2   = tax.createNode(null, "grandchild2");
+            var gg   = tax.createNode(null, "ggrandchild");
+            var gg2  = tax.createNode(null, "ggrandchild2");
+            var gg3  = tax.createNode(null, "ggrandchild3");
+            tax.addNode( gg, tax.addNode(g, tax.addNode(c, tax.addNode(p, null))));
+            tax.addNode( gg2, tax.addNode(g2, tax.addNode(c2, p)));
+            tax.addNode( gg3, g);
+            var path = null;
+            path = tax.path(gg._id);
+            path.should.eql('/parent/child/grandchild/');
+            path = tax.path(gg2._id);
+            path.should.eql('/parent/child2/grandchild2/');
+            path = tax.path(gg3._id);
+            path.should.eql('/parent/child/grandchild/');
+            done();
+        });
+        it('should be able to get the path of deeply nested node', function(done) {
+            var path = null, i, n, arr;
+            arr = [];
+            for(i=1; i<=10;i++) {
+                n = tax.createNode(null, i);
+                tax.addNode(n, arr.pop());
+                arr.push(n);
+            }
+            path = tax.path(arr.pop()._id);
+            path.should.eql('/1/2/3/4/5/6/7/8/9/');
+            done();
+        });
+    });
+});
